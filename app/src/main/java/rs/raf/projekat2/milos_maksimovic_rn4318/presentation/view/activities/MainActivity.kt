afd4ca0fef7c471d199cae35ade821d168f419cf
Maksa <milosmilos@ymail.com>
2021-05-26 16:12:22 +0200

    added dependencies for the project

--- app/src/main/java/rs/raf/projekat2/milos_maksimovic_rn4318/presentation/view/activities/MainActivity.kt
+++ app/src/main/java/rs/raf/projekat2/milos_maksimovic_rn4318/presentation/view/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package rs.raf.projekat2.milos_maksimovic_rn4318
+package rs.raf.projekat2.milos_maksimovic_rn4318.presentation.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import rs.raf.projekat2.milos_maksimovic_rn4318.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
