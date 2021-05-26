8bf29091543417ad5d099cb6a2d12e8999648ba5
Maksa <milosmilos@ymail.com>
2021-05-26 13:11:51 +0200

    init commit

---
+++ app/src/main/java/rs/raf/projekat2/milos_maksimovic_rn4318/MainActivity.kt
@@ -0,0 +1,11 @@
+package rs.raf.projekat2.milos_maksimovic_rn4318
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
